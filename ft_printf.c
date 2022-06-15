03d8d79f268410aba47e2b73c745b0b3af4a9de1
Kenzy YACINI <kyacini@e1r1p13.42.fr>
2022-06-15 06:05:58 +0200

    test

--- ft_printf.c
+++ ft_printf.c
@@ -1,125 +1,22 @@
-#include <stdarg.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyacini <kyacini@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/15 01:46:26 by kyacini           #+#    #+#             */
+/*   Updated: 2022/06/15 05:22:22 by kyacini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
 #include <stdio.h>
-
-int tranlate_c(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
-
-void	put_unsignednbr(unsigned int nb)
-{
-	if (nb > 9)
-	{
-		put_unsignednbr(nb / 10);
-		put_unsignednbr(nb % 10);
-	}
-	if (nb <= 9)
-	{
-		tranlate_c(nb + '0');
-	}
-}
-
-void	put_unsignednbrsss(unsigned long int nb)
-{
-	if (nb > 9)
-	{
-		put_unsignednbr(nb / 10);
-		put_unsignednbr(nb % 10);
-	}
-	if (nb <= 9)
-	{
-		tranlate_c(nb + '0');
-	}
-}
-
-void	conv_hex(int nb, char minmaj)
-{
-	char	*hex;
-	if(minmaj == 'X')
-		hex = "0123456789ABCDF";
-	else
-		hex = "0123456789abcdef";
-	if (nb > 16)
-	{
-		conv_hex(nb / 16, minmaj);
-		conv_hex(nb % 16, minmaj);
-	}
-	else
-		tranlate_c(hex[nb]);
-}
-
-int	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		tranlate_c(str[i]);
-		i++;
-	}
-	return (i);
-}
-
-void	ft_putnbr(int nb)
-{
-	if (nb == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-	}
-	if (nb < 0 && nb != -2147483648)
-	{
-		write(1, "-", 1);
-		nb *= -1;
-	}
-	if (nb > 9 && nb != -2147483648)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	if (nb <= 9 && nb != -2147483648)
-	{
-		tranlate_c(nb + '0');
-	}
-}
-
-int	compte_chiffre(int nb, int base)
-{
-	int	i;
-
-	i = 0;
-	if (nb == 0)
-		return (1);
-	while (nb != 0)
-	{
-		nb /= base;
-		i++;
-	}
-	return (i);
-}
-
-int	translate_i_d(int val)
-{
-	ft_putnbr(val);
-	return (compte_chiffre(val, 10));
-}
-
-int translate_u(unsigned int val)
-{
-	put_unsignednbr(val);
-	return (compte_chiffre(val, 10));
-}
-
-int	translate_x_X(int val, char str)
-{
-	conv_hex(val, str);
-	return (compte_chiffre(val, 16));
-}
 
 void	parcer(char str, va_list val, int *count)
 {
+	unsigned long int	temp;
+
 	if (str == 'c')
 		*count = *count + tranlate_c(va_arg(val, int));
 	else if (str == 's')
@@ -127,26 +24,30 @@ void	parcer(char str, va_list val, int *count)
 	else if (str == 'd' || str == 'i')
 		*count = *count + translate_i_d(va_arg(val, int));
 	else if ((str == 'x' || str == 'X'))
-		*count = *count + translate_x_X(va_arg(val, int), str);
+		conv_hex(va_arg(val, int), str, count);
+	else if (str == 'p')
+	{
+		temp = va_arg(val, unsigned long int);
+		*count = *count + ft_putstr("0x");
+		conv_hexadd(temp);
+		*count = *count + temp;
+	}
 	else if (str == 'u')
-		*count = *count + translate_u(va_arg(val, unsigned int));
+		put_unsignednbr(va_arg(val, unsigned int), count);
 	else if (str == '%')
 		*count = *count + tranlate_c('%');
 	else
-	{
-		tranlate_c('%');
-		tranlate_c(str);
-		*count = *count + 2;
-	}
+		*count = tranlate_c('%') + tranlate_c(str);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
-	int	i;
-	int	count;
+	int		i;
+	int		count;
 
 	i = 0;
+	count = 0;
 	va_start(args, str);
 	while (str[i])
 	{
@@ -168,8 +69,17 @@ int	ft_printf(const char *str, ...)
 
 int main()
 {
-	int a = 7;
-	int *p = &a;
-	put_unsignednbr(p);
-	//printf("%d\n" ,ft_printf("%x  %%% %X", 3457, 3457));
+	int i = 0, j = 0;
+
+	void* p2 = NULL;
+	//int p2 = NULL;
+	i = ft_printf("ft_printf :    %p\n", p2);
+	j = printf("    printf :   %p\n", p2);
+	// ssssss
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	// ssss
+	printf("L ADRESSE -------------------\n");
+	ft_printf("ft_printf :    %p\n", &p2);
+	printf("    printf :   %p\n", &p2);
 }
